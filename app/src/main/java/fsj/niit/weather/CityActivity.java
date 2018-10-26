419f998c4deebd1884067dc31402a193293e4ae4
fsj0522 <1300653421@qq.com>
2018-10-26 16:58:09 +0800

    update

---
+++ app/src/main/java/fsj/niit/weather/CityActivity.java
@@ -0,0 +1,18 @@
+package fsj.niit.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+    }
+
+    public void onClick(View view) {
+
+    }
+}
--- app/src/main/java/fsj/niit/weather/WeatherActivity.java
+++ app/src/main/java/fsj/niit/weather/WeatherActivity.java
@@ -1,13 +1,34 @@
 package fsj.niit.weather;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        btnHome=findViewById(R.id.btn_home);
+
+        btnHome.setOnClickListener(this);
     }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_home:
+                Intent intentCity=new Intent(WeatherActivity.this,CityActivity.class);
+                //intentLogin.putExtra("username",name);
+                startActivity(intentCity);
+                break;
+        }
+    }
+
+
 }
